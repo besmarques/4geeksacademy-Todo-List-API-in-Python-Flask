ac7454b578453d7968815f1cab25e198be817065
Bruno Marques <besmarques@gmail.com>
2022-02-08 18:19:28 +0000

    done

--- src/app.py
+++ src/app.py
@@ -6,6 +6,8 @@ todos = [
     { "label": "My first task", "done": False }
 ]
 
+#request_body = "teteste"
+
 @app.route('/todos', methods=['GET'])
 def hello_world():
     json_text = jsonify(todos)
@@ -13,11 +15,14 @@ def hello_world():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.json
+    request_body = request.get_json(force=True)
+    todos.append(request_body)
+    return jsonify(todos), 200
 
-    
-    
-    return jsonify(todos)
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.remove(todos[position])
+    return jsonify(todos), 200
 
 # These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
